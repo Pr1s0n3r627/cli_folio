d846640ed870d53329fe9242dbd473531d49b27a
Kshitij Sahdev <85095194+Pr1s0n3r627@users.noreply.github.com>
2024-09-02 18:34:42 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      '@': path.resolve(__dirname, "./src"),
-    }
+    // alias: {
+    //   '@': path.resolve(__dirname, "./src"),
+    // }
   }
 })
